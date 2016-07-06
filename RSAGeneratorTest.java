3b94d2f6ba0c0d1c477ffd117c5267affb4a175b
Christopher Life <clife@uvic.ca>
2016-07-05 19:23:28 -0700

    Finished JUnit Tests
    Finished JUnit tests for the RSAGenerator class

--- RSAGeneratorTest.java
+++ RSAGeneratorTest.java
@@ -9,11 +9,13 @@ import java.security.NoSuchAlgorithmException;
 public class TestRSAGenerator {
    @Test
    public void RSAGenerator() {
+	   //Test the RSAGenerator() method by generating an RSA generator class instance then checking it's not null
       RSAGenerator keyGen = new RSAGenerator();
       assertNotNull(keyGen);
    }
    @Test
    public void TestGenerateRSAKeys() {
+	   //Test the RSAGenerator() method by generating an RSA key pair key then checking it's not null
       RSAGenerator keyGen = new RSAGenerator();
       try {
 		KeyPair RSAKeys = keyGen.GenerateRSAKeys();
