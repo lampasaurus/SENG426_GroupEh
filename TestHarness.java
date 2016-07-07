3469b4b900e4c4f78eeeaf3ba4abaff80e461b0f
Christopher Life <clife@uvic.ca>
2016-07-07 14:07:01 -0700

    Updated test harness
    Runs AESCrypt and RSA Generator tests. Other classes need to be added via similar syntax when finalized

--- TestHarness.java
+++ TestHarness.java
@@ -1,13 +1,47 @@
+package eCheque;
+
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class TestRunner {
    public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(TestJunit.class);
-      for (Failure failure : result.getFailures()) {
-         System.out.println(failure.toString());
+ 	  System.out.println("JUnit test output for E-Cheque System");
+	  //Instantiate result objects
+	  //ONE RESULT OBJECT PER CLASS TO BE TESTED
+	  Result resultRSAGenerator = JUnitCore.runClasses(TestRSAGenerator.class);
+	  Result resultAESCrypt = JUnitCore.runClasses(TestAESCrypt.class);
+	  
+	  //Print out results
+	  //ONE 'FOR' LOOP CHUNK PER TESTED CLASS
+//RSAGenerator
+      for (Failure failureRSAGenerator : resultRSAGenerator.getFailures()) {
+          System.out.println(failureRSAGenerator.toString());
       }
-      System.out.println(result.wasSuccessful());
+      System.out.println("CLASS: RSAGenerator");
+      System.out.println("  TESTS RUN: " + resultRSAGenerator.getRunCount());
+      System.out.println("  TESTS PASSED: " + (resultRSAGenerator.getRunCount()-resultRSAGenerator.getFailureCount()));
+      System.out.println("  TESTS FAILED: " + resultRSAGenerator.getFailureCount());
+      if(resultRSAGenerator.wasSuccessful()){
+    	  System.out.println("  RESULT: SUCCESS");
+      }
+      else{
+    	  System.out.println("  RESULT: FAILURE");
+      }
+      
+//AESCrypt      
+      for (Failure failureAESCrypt : resultAESCrypt.getFailures()) {
+          System.out.println(failureAESCrypt.toString());
+      }
+      System.out.println("CLASS: AESCrypt");
+      System.out.println("  TESTS RUN: " + resultAESCrypt.getRunCount());
+      System.out.println("  TESTS PASSED: " + (resultAESCrypt.getRunCount()-resultAESCrypt.getFailureCount()));
+      System.out.println("  TESTS FAILED: " + resultAESCrypt.getFailureCount());
+      if(resultAESCrypt.wasSuccessful()){
+    	  System.out.println("  RESULT: SUCCESS");
+      }
+      else{
+    	  System.out.println("  RESULT: FAILURE");
+      }
    }
-}
+}  	
